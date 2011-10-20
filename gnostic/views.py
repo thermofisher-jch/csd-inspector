789ee809ec8336eb26f0c96bf7f44fe7d6406ffa
Brian Kennedy <Brian.Kennedy2@lifetech.com>
2011-10-20 14:27:29 -0700

    Initial routes SQLAlchemy Pyramid project commit.

---
+++ gnostic/views.py
@@ -0,0 +1,7 @@
+from gnostic.models import DBSession
+from gnostic.models import MyModel
+
+def my_view(request):
+    dbsession = DBSession()
+    root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
+    return {'root':root, 'project':'gnostic'}
