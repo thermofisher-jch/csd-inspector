db4f9a77f89d40b776e2501e2ac87f435b8a3e89
Brian Kennedy <Brian.Kennedy2@lifetech.com>
2011-10-24 19:01:57 -0700

    Celery configuration changes in order to log everything.

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,8 +1,11 @@
-CELERY_IGNORE_RESULT = True
+CELERY_IGNORE_RESULT = False
 CELERY_RESULT_BACKEND = "database"
 CELERY_RESULT_DBURI = "sqlite:///celery_result.db"
 
 BROKER_TRANSPORT = "sqlakombu.transport.Transport"
 BROKER_HOST = "sqlite:///celery_broker.db"
 
-CELERY_IMPORTS = ("upload", )
+CELERY_IMPORTS = ("gnostic.upload", )
+
+CELERYD_LOG_FILE = "celeryd.log"
+CELERYD_LOG_LEVEL = "DEBUG"
