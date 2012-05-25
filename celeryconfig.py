9e7dd31715f525d0d75b41aa7c5027e38123ba8f
Brian Kennedy <Brian.Kennedy2@lifetech.com>
2012-05-24 19:45:18 -0700

    Switching Celery to a RabbitMQ backend

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,11 +1,24 @@
+# Name of nodes to start
+# here we have a single node
+CELERYD_NODES="worker1"
+
 CELERY_IGNORE_RESULT = False
 CELERY_RESULT_BACKEND = "database"
 CELERY_RESULT_DBURI = "sqlite:///celery_result.db"
 
-BROKER_TRANSPORT = "sqlakombu.transport.Transport"
-BROKER_HOST = "sqlite:///celery_broker.db"
+BROKER_URL = "amqp://lemon:lemonpass@localhost:5672/lemon"
 
 CELERY_IMPORTS = ("lemontest.upload", "lemontest.diagnostic")
 
-CELERYD_LOG_FILE = "celeryd.log"
-CELERYD_LOG_LEVEL = "DEBUG"
+# Extra arguments to celeryd
+CELERYD_OPTS="--time-limit=300 --concurrency=8"
+
+# Name of the celery config module.
+CELERY_CONFIG_MODULE="celeryconfig"
+
+# %n will be replaced with the nodename.
+CELERYD_PID_FILE="/var/run/celery/%n.pid"
+
+# Workers should run as an unprivileged user.
+CELERYD_USER="celery"
+CELERYD_GROUP="celery"
