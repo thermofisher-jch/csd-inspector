681f4e09fd170d252c6784021feaa569c7427d73
Brian Kennedy <Brian.Kennedy2@lifetech.com>
2012-12-14 16:43:44 -0800

    Adding better local development config.
    I switched to using RabbitMQ locally because kombu was a hot mess before celery 3.0, and upgrading to celery 3.0 is currently presenting it's own unique set of challenges...

--- celeryconfig_development.py
+++ celeryconfig_development.py
@@ -1,23 +1,22 @@
 # Name of nodes to start
 # here we have a single node
-CELERYD_NODES="celery"
+CELERYD_NODES="lemon"
 
-CELERY_IGNORE_RESULT = False
 CELERY_RESULT_BACKEND = "database"
 CELERY_RESULT_DBURI = "sqlite:///celery_result.db"
 
-BROKER_URL = "amqp://lemon:lemonpass@localhost:5672/lemon"
+BROKER_URL = 'amqp://guest:guest@localhost'
 
 CELERY_IMPORTS = ("lemontest.upload", "lemontest.diagnostic")
 
 # Extra arguments to celeryd
-CELERYD_OPTS="--time-limit=300 --concurrency=8"
+CELERYD_OPTS=""
 
 # Name of the celery config module.
-CELERY_CONFIG_MODULE="celeryconfig"
+CELERY_CONFIG_MODULE="celeryconfig_development"
 
 # %n will be replaced with the nodename.
-CELERYD_PID_FILE="/var/run/celery/%n.pid"
+CELERYD_PID_FILE="%n.pid"
 
 # Workers should run as an unprivileged user.
 CELERYD_USER="celery"
