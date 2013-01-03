4db153a800b1ec4e847e96313285b2172a372559
Brian Kennedy <Brian.Kennedy2@lifetech.com>
2013-01-02 17:02:05 -0800

    Case insensitive auto-complete matching

--- lemontest/api.py
+++ lemontest/api.py
@@ -8,12 +8,12 @@ from lemontest.models import Archive
 @view_config(route_name="api_auto_complete", renderer="json")
 def auto_name(request):
     what = request.GET.get("what")
-    match = request.GET.get("match")
+    match = '%%%s%%' % request.GET.get("match")
     objects = {
         "name": Archive.submitter_name,
         "site": Archive.site
     }
     field = objects.get(what)
-    matches = DBSession.query(field).filter(field.contains(match)).distinct().limit(8)
+    matches = DBSession.query(field).filter(field.ilike(match)).distinct().limit(8)
     return [m[0] for m in matches]
 
