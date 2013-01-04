401621a9d9d9aae864ce2fe00f133f25a1dff77a
Brian Kennedy <Brian.Kennedy2@lifetech.com>
2013-01-04 15:22:09 -0800

    IO-21 Version Check test added.

---
+++ lemontest/diagnostics/Version_Check/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+
+version_threshold = '3.0'
+
+if __name__ == "__main__":
+    outpath = sys.argv[1]
+    version_path = os.path.join(outpath, "version.txt")
+    if os.path.exists(version_path):
+        line = open(version_path).readline()
+        version = line.split('=')[-1].strip()
+        if version >= version_threshold:
+            print("OK")
+            print("0")
+            print("Version is acceptable at %s" % version)
+        else:
+            print("Fail")
+            print("75")
+            print("Advise customer to upgrade their Torrent Server.  Their version is out-dated at %s" % version)
+    else:
+        raise IOError("No version.txt included")
