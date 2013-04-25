23729b662b53f725bd4f18e321a536a09d50b239
Inspector <inspector@Antonios-MacBook-Pro.local>
2013-04-24 22:15:24 -0700

    Changes to process OT1 & OT2 plots

--- lemontest/diagnostics/Plots/OT_plots.R
+++ lemontest/diagnostics/Plots/OT_plots.R
@@ -1,43 +1,58 @@
 OT_plots<-function(work_dir,file_name,machine,target,version) {
     
     
+    OT2=FALSE
+    OT1=FALSE
 
-    # type_of_data<-strsplit(file_name,"_")
-    
-    # machine<-type_of_data[[1]][1]
-    #machine= "machine"
-    # target<-type_of_data[[1]][2]
-    #target="target"
     dataSet <- read.csv(paste(work_dir,file_name,sep=""),header=TRUE)
 
-    #check the file is healthy; 
-    #vector "header" contains the expected header of the file
+    #check if the file is healthy; 
+    header_len=length(names(dataSet))
 
-    header=c("Step","Thermistor.0.Temperature","Thermistor.1.Temperature","Thermistor.2.Temperature",
-        "Thermistor.3.Temperature","Thermistor.4.Temperature","P_Sensor..Cur..Pressure","Raw.A2D",
-        "TEC.0.Current.Temp","Set.Temp","PWM.duty.Cycle","On.Time","Tec.Dir","Adjusted.Set.Temp",
-        "Heater.0.Current.Temp","Set.Temp.1","PWM.duty.Cycle.1","Heater.1.Current.Temp","Set.Temp.2",
-        "PWM.duty.Cycle.2","Motor.Power..Oscillation","Raw","Solenoid.Pump..Total.Volume",
-        "Dispensed.Volume","Oil.Pump.Status","Sample.Pump.Status","Pin.0.Pin","Pin.1.Pin",
-        "Pin.2.Pin","Pin.3.Pin","Pin.4.Pin","Pin.5.Pin","Flowmeter0","Timestamp")
 
-    #stop if the header of the file is not as expected
-    if(! all(names(dataSet)==header)) {
+    #There are 2 types of file:
+    #OT2 has 34 columns
+    if( header_len ==34){
+        OT2=TRUE;
+        machine="OT2"
+        header=c("Step","Thermistor0Temperature","Thermistor1Temperature","Thermistor2Temperature","Thermistor3Temperature",
+            "Thermistor4Temperature","P_SensorCur.Pressure","RawA2D","TEC0CurrentTemp","SetTemp","PWMdutyCycle","OnTime",
+            "TecDir","AdjustedSetTemp","Heater0CurrentTemp","SetTemp.1","PWMdutyCycle.1","Heater1CurrentTemp","SetTemp.2",
+            "PWMdutyCycle.2","MotorPowerOscillation","Raw","SolenoidPumpTotalVolume","DispensedVolume","OilPumpStatus",
+            "SamplePumpStatus","Pin0Pin","Pin1Pin","Pin2Pin","Pin3Pin","Pin4Pin","Pin5Pin","Flowmeter0","Timestamp")
+        #stop if the header names of the file are not as expected for OT2
+        if( ! (all(names(dataSet)==header ))) {
+            cat("Stopping: the file header is different from what is expected\n")
+            cat("Warning\n");
+            cat("30\n");
+            stop()
+        }
+    }else if(header_len ==32) {
+        OT1=TRUE;
+        machine="OT1"
+        target=""
+        version=""
+        #OT1 has 32 columns and does not have pump data
+        header=c("Thermistor0Temperature","Thermistor1Temperature","Thermistor2Temperature","Thermistor3Temperature",
+            "Thermistor4Temperature","Heater0CurrentTemp","SetTemp","PWMdutyCycle","Heater1CurrentTemp","SetTemp.1",
+            "PWMdutyCycle.1","Heater2CurrentTemp","SetTemp.2","PWMdutyCycle.2","Heater3CurrentTemp","SetTemp.3",
+            "PWMdutyCycle.3","PressureAdj.SetPt","SetPt","P_SensorCur.Pressure","RawA2D","MotorPower.Oscillation",
+            "Valve0","Valve1","Valve2","Valve3","Valve4","Valve5","Valve6","Fan0","Fan1","Timestamp")
+
+        #stop if the header names of the file are not as expected for OT1
+        if( ! (all(names(dataSet)==header))) {
+            cat("Stopping: the file header is different from what is expected\n")
+            cat("Warning\n");
+            cat("30\n");
+            stop()
+        }
+    }else{
         cat("Warning\n");
         cat("30\n");
-        cat("Stopping: the file header is different from what is expected\n")
-        cat("Verify that the header and structure of your file is exactly as follows:\n\n")
-        cat(paste("Step,Thermistor-0 Temperature,Thermistor-1 Temperature,Thermistor-2 Temperature,",
-            "Thermistor-3 Temperature,Thermistor-4 Temperature,P_Sensor- Cur. Pressure, ",
-            "Raw A2D,TEC-0 Current Temp, Set Temp, PWM duty Cycle, On Time, Tec Dir, ",
-            "Adjusted Set Temp,Heater-0 Current Temp, Set Temp, PWM duty Cycle,Heater-1 Current Temp, ",
-            "Set Temp, PWM duty Cycle,Motor Power- Oscillation, Raw,Solenoid Pump- Total Volume, ",
-            "Dispensed Volume, Oil Pump Status, Sample Pump Status,Pin-0 Pin,Pin-1 Pin,Pin-2 Pin,",
-            "Pin-3 Pin,Pin-4 Pin,Pin-5 Pin,Flowmeter0,Timestamp",sep=""))
-        cat("The file's header is different from what was expected\n");
+        cat("The file's header length is different from what is expected")
         stop()
     }
-    
+  
     #The name of the resulting report
     mainHTML<-"report.html"
     
@@ -45,6 +60,7 @@ OT_plots<-function(work_dir,file_name,machine,target,version) {
     
     #make it nicer by adding a css file
     cat("<html><link rel=stylesheet href=some.css type=text/css>\n</head><body>\n",file=f,append=FALSE)
+
     cat(paste("<h1 align=\"center\">Plots for",machine,target,version,"</h1>\n",sep=" "),file=f,append=TRUE)
 
     #get the number of rows to be plotted
@@ -52,60 +68,57 @@ OT_plots<-function(work_dir,file_name,machine,target,version) {
     
     #This is plot 1
     #verify that the data can be plotted
-    if(!(is.numeric(dataSet$Thermistor.0.Temperature) & is.numeric(dataSet$Thermistor.1.Temperature) & is.numeric(dataSet$Thermistor.2.Temperature) & is.numeric(dataSet$Thermistor.3.Temperature))) {
+    if(!(is.numeric(dataSet$Thermistor0Temperature) & is.numeric(dataSet$Thermistor1Temperature) & is.numeric(dataSet$Thermistor2Temperature) & is.numeric(dataSet$Thermistor3Temperature))) {
         cat("<h3 align=\"center\">The data temperatures cannot be plotted: verify all the data are numeric</h3>\n",sep=" ",file=f,append=TRUE)  
         cat("The data temperatures cannot be plotted: verify all the data are numeric")
     }else{
         graph <- paste(work_dir,"p1.png",sep="")
         png(graph)
 
-        plot(seq(1,l)/60,ylim=(c(0,100)),dataSet$Thermistor.0.Temperature,las=1, yaxp= c(0,100,10), type="l",col="dark green",xlab="Time (minutes)",ylab="Temperature (Celsius)",main="Thermistor Temperature")
+        plot(seq(1,l)/60,ylim=(c(0,100)),dataSet$Thermistor0Temperature,las=1, yaxp= c(0,100,10), type="l",col="dark green",xlab="Time (minutes)",ylab="Temperature (Celsius)",main="Thermistor Temperature")
 
-        lines(seq(1,l)/60,dataSet$Thermistor.1.Temperature,type="l",col="dark blue")
-        lines(seq(1,l)/60,dataSet$Thermistor.2.Temperature,type="l",col="red")
-        lines(seq(1,l)/60,dataSet$Thermistor.3.Temperature,type="l",col="orange")
+        lines(seq(1,l)/60,dataSet$Thermistor1Temperature,type="l",col="dark blue")
+        lines(seq(1,l)/60,dataSet$Thermistor2Temperature,type="l",col="red")
+        lines(seq(1,l)/60,dataSet$Thermistor3Temperature,type="l",col="orange")
         legend("bottomright",cex=0.8,c("T0-Ambient","T1-Heated Lid","T2-Thermal Cycling","T3-Internal Case"),col=c("dark green","dark blue","red","orange"),lty=1)
         dev.off()
-
         cat("<img src=\"p1.png\" />", file=f, append=TRUE)
     }
 
     #This is plot 2
-    #verify the data can be ploted
-    if(!(is.numeric(dataSet$P_Sensor..Cur..Pressure))) {
+    #verify the data can be plotted
+    if(!(is.numeric(dataSet$P_SensorCur.Pressure))) {
         cat("<h3 align=\"center\">The sensor pressure data cannot be plotted: the data is not numeric</h3>\n",sep=" ",file=f,append=TRUE)
         cat("The sensor pressure data cannot be plotted: the data is not numeric")
     }else{
         graph <- paste(work_dir,"p2.png",sep="")
         png(graph)
-        plot(seq(1,l)/60,las=1,dataSet$P_Sensor..Cur..Pressure,type="l",col="dark blue",xlab="Time (minutes)",ylab="Pressure (PSI)",main="Sensor Presure")
+        plot(seq(1,l)/60,las=1,dataSet$P_SensorCur.Pressure,type="l",col="dark blue",xlab="Time (minutes)",ylab="Pressure (PSI)",main="Sensor Presure")
+
         dev.off()
 
         cat("<img src=\"p2.png\" /><br />", file=f, append=TRUE)
     }
 
-    #Check if there is an error (a value is 5) in the pump status
-    if(!(is.numeric(dataSet$P_Sensor..Cur..Pressure))) {
-        cat("<h3 align=\"center\">Unable to test Sample pump status: the data is not numeric</h3>\n",sep=" ",file=f,append=TRUE)
-        cat("Unable to test Sample pump status: the data is not numeric")
-    }else{
-        
-        if(5 %in% dataSet$Sample.Pump.Status){
-            cat("<h2 align=\"center\" style=\"color: red\">Sample pump status test: ERROR detected value of 5</h3>\n",sep=" ",file=f,append=TRUE)
-        }else{
-            cat("<h2 align=\"center\" style=\"color: lime\">Sample pump status test: OK</h3>\n",sep=" ",file=f,append=TRUE)
-        }
-    }
-            
+	if(OT2==TRUE){
+		#Check if there is an error (a value is 5) in the pump status
+		if(!(is.numeric(dataSet$SamplePumpStatus))) {
+			cat("<h3 align=\"center\">Unable to test Sample pump status: the data is not numeric</h3>\n",sep=" ",file=f,append=TRUE)
+			cat("Unable to test Sample pump status: the data is not numeric")
+		}else{		
+			if(5 %in% dataSet$SamplePumpStatus){
+				cat("<br></br><br></br> <h2 align=\"center\" style=\"color: red\">Sample pump status test: ERROR detected value of 5</h3>\n",sep=" ",file=f,append=TRUE)
+			}else{
+				cat("<h2 align=\"center\" style=\"color: lime\">Sample pump status test: OK</h3>\n",sep=" ",file=f,append=TRUE)
+			}
+		}
+	}
     #finish report
     cat("\n<hr size=1>\n</body></html>",file=f,append=TRUE)
     cat("Info\n");
     cat("20\n");
-    
-
 }
 
 cmd.args <- commandArgs(trailingOnly = TRUE);
 OT_plots(cmd.args[1], cmd.args[2], cmd.args[3], cmd.args[4], cmd.args[5]);
-
 
