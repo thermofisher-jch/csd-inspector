aee2a82a5bfe8e208c318af2caabff9372a2c6e5
Brian Kennedy <Brian.Kennedy2@lifetech.com>
2014-02-20 15:15:06 -0800

    Initial flow plot draft

---
+++ lemontest/diagnostics/Flow/logs.mako
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<link rel="stylesheet" href="/static/css/bootstrap.css"/>
+	<style type="text/css">
+		#files {
+			font-family: monospace;
+		}
+	</style>
+</head>
+<body>
+<img src="plot.png" />
+</body>
+</html>
---
+++ lemontest/diagnostics/Flow/main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import os.path
+from mako.template import Template
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+plt.ioff()
+
+if __name__ == "__main__":
+    archive, output = sys.argv[1:3]
+    file_count = 0
+    files = []
+    headers = []
+    warnings = []
+    csv_path = ''
+    rel_csv_path = ''
+    errors = []
+    
+    for path, dirs, names in os.walk(archive):
+        if "test_results" not in path:
+            for name in names:
+                if "logs.tar" not in name:
+                    rel_dir = os.path.relpath(path, archive)
+                    rel = os.path.join(rel_dir, name)
+                    full = os.path.join(path, name)
+                    files.append(rel)
+                    file_count += 1
+                    if rel.startswith("var/log/IonChef/RunLog/") and rel.endswith(".csv"):
+                        csv_path = full
+                        res_csv_path = rel
+
+    if csv_path:
+        with open(csv_path) as csvfile:
+            reader = csv.reader(csvfile)
+            header = reader.next()
+            cols = [(float(r[0]), int(r[7])) for r in reader]
+            time, flows = zip(*cols)
+        plt.plot(data)
+        figure_path = os.path.join(output, "plot.png")
+        plt.savefig()
+
+    context = {}
+    template = Template(filename="logs.mako")
+    result = template.render(**context)
+    with open(os.path.join(output, "results.html"), 'w') as out:
+        out.write(result.encode("UTF-8"))
+    summary = ""
+    print("Info")
+    print("20")
