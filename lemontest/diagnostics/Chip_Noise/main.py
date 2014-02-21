be45611490cedbf5b3114705e63a54387d285162
Brian Kennedy <Brian.Kennedy2@lifetech.com>
2014-02-20 17:43:10 -0800

    Fixing chip noise file validation

--- lemontest/diagnostics/Chip_Noise/main.py
+++ lemontest/diagnostics/Chip_Noise/main.py
@@ -21,10 +21,15 @@ def validate(archive_path):
         return "explog_final.txt missing", False
 
     explog = load_explog(path)
-    if "Noise_90pct" not in explog:
-        return "Noise_90pct missing from explog_final.txt", False
+
     if "ChipType" not in explog:
         return "ChipType missing from explog_final.txt", False
+    chip_type = data["ChipType"][:3]
+    if chip_type != '900' and "Noise_90pct" not in explog:
+        return "Noise_90pct missing from explog_final.txt", False
+    elif chip_type == '900' and "ChipNoiseInfo" not in explog:
+        return "ChipNoiseInfo missing from explog_final.txt", False
+
     return explog, True
 
 
