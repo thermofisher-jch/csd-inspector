2f71524a512e5c360c23a30f3a4660ad55fe7daf
Brian Kennedy <Brian.Kennedy2@lifetech.com>
2014-06-05 15:13:30 -0700

    IO-84 Adding seq kit test to PGM and Proton

---
+++ lemontest/diagnostics/Seq_Kit/main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sys
+import os
+
+
+def load_explog(path):
+    data = {}
+    for line in open(path):
+        # Trying extra hard to accomodate formatting issues in explog
+        datum = line.split(":", 1)
+        if len(datum) == 2:
+            key, value = datum
+            data[key.strip()] = value.strip()
+    return data
+
+
+def validate(archive_path):
+    path = os.path.join(archive_path, "explog_final.txt")
+    if not os.path.exists(path):
+        return "explog_final.txt missing", data, False
+
+    explog = load_explog(path)
+    data = {}
+    data['inspector_seq_kit'] = explog.get("SeqKitDesc", None)
+    if not data['inspector_seq_kit']:
+        data['inspector_seq_kit'] = explog.get("SeqKitPlanDesc", None)
+        if not data['inspector_seq_kit']:
+            return "Seq Kit missing from explog_final.txt", data, False
+    return explog, data, True
+
+
+def report(data):
+    kit_name = data.get("inspector_seq_kit")
+    if "IC" in kit_name:
+        print("Info")
+        print(20)
+        print(u"Chef kit, {}".format(kit_name))
+    else:
+        print("Info")
+        print(20)
+        print(u"OneTouch kit, {}".format(kit_name))
+        
+
+def main():
+    archive_path, output_path = sys.argv[1:3]
+    raw, data, valid = validate(archive_path)
+    if not valid:
+        print("N\A")
+        print(0)
+        print(raw)
+        sys.exit()
+    else:
+        report(data)
+
+main()
