2573586ec27ccea6da246f190d2510b7b13a918e
Brian Kennedy <Brian.Kennedy2@lifetech.com>
2014-10-20 17:01:51 -0700

    Modifying migration task to ignore existing entries

--- lemontest/metrics_migration.py
+++ lemontest/metrics_migration.py
@@ -8,17 +8,20 @@ from celery.task import task
 
 from lemontest.models import DBSession
 from lemontest.models import Archive
-from lemontest.models import MetricsPGM
-from lemontest.models import MetricsProton
+from lemontest.models import MetricsPGM, MetricsProton, MetricsOTLog
 
 from lemontest.upload import set_metrics_pgm
 from lemontest.upload import set_metrics_proton
+
 
 @task
 def metrics_migration(archive_id):
     archive = DBSession.query(Archive).get(archive_id)
 
+
     if archive.archive_type == "PGM_Run":
+        if DBSession.query(MetricsPGM).filter_by(archive_id=archive_id).count():
+            return
         metrics_pgm = MetricsPGM()
         metrics_pgm.archive_id = archive_id
         DBSession.add(metrics_pgm)
@@ -26,6 +29,8 @@ def metrics_migration(archive_id):
         set_metrics_pgm.delay(metrics_pgm.id)
 
     elif archive.archive_type == "Proton":
+        if DBSession.query(MetricsProton).filter_by(archive_id=archive_id).count():
+            return
         metrics_proton = MetricsProton()
         metrics_proton.archive_id = archive_id
         DBSession.add(metrics_proton)
@@ -33,6 +38,8 @@ def metrics_migration(archive_id):
         set_metrics_proton.delay(metrics_proton.id)
 
     elif archive.archive_type == "OT_Log":
+        if DBSession.query(MetricsOTLog).filter_by(archive_id=archive_id).count():
+            return
         metrics_otlog = MetricsOTLog()
         metrics_otlog.archive_id = archive_id
         DBSession.add(metrics_otlog)
