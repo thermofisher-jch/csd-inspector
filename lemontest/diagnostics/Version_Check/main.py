d35c74a88c878651fb2d7c930af5c7e98835e4fd
Brian Bourke-Martin <brian.bourke-martin@lifetech.com>
2015-10-08 10:36:54 -0400

    IO-119 * Updated the Version Check with logic a'la the ticket comments * Added better version number parsing via usage of the class distutils.version.StrictVersion * Added new required libraries to setup.py

--- lemontest/diagnostics/Version_Check/main.py
+++ lemontest/diagnostics/Version_Check/main.py
@@ -2,12 +2,10 @@
 
 import sys
 import os
+from distutils.version import StrictVersion
 
-
-version_threshold = '3.0'
-
-proton_version = 'Proton Release'
-pgm_version    = 'PGM SW Release'
+OK_STRING = "TS Version is acceptable at <strong>%s</strong>"
+ALERT_STRING = "Advise customer to upgrade their Torrent Server.  Their version is out-dated at <strong>%s</strong>"
 
 def load_explog(path):
     data = {}
@@ -26,32 +24,65 @@ def validate(archive_path):
         return "explog_final.txt missing", False
 
     explog = load_explog(path)
-    if pgm_version not in explog and proton_version not in explog:
-        return "Instrument version missing from explog_final.txt", False
-
     return explog, True
 
 
+def printResult(alert, msg):
+    """
+    Helper method to print the alert statement
+    :param alert: boolean if this is an alert
+    :param msg: The message to be printed
+    """
+    print("Alert" if alert else "OK")
+    print("40" if alert else "10")
+    print(msg)
+
 if __name__ == "__main__":
     outpath = sys.argv[1]
+    explog, valid = validate(outpath)
+    if not valid:
+        print("N/A")
+        print("0")
+        print "Could not find a valid explog."
+        exit()
+
     version_path = os.path.join(outpath, "version.txt")
-    if os.path.exists(version_path):
-        line = open(version_path).readline()
-        version = line.split('=')[-1].strip()
-        if version >= version_threshold:
-            print("OK")
-            print("10")
-            print("TS Version is acceptable at <strong>%s</strong>" % version)
-        else:
-            print("Alert")
-            print("40")
-            print("Advise customer to upgrade their Torrent Server.  Their version is out-dated at <strong>%s</strong>" % version)
-        data, valid = validate(outpath)
-        if valid:
-            version = data.get(pgm_version, None) or data.get(proton_version, None)
-            if version:
-                print("  Instrument version is <strong>%s</strong>" % version)
-    else:
+    if not os.path.exists(version_path):
         print("N/A")
         print("0")
         print("No version.txt included")
+        exit()
+
+    # get the version number
+    line = open(version_path).readline()
+    version = line.split('=')[-1].strip()
+    version = version.split()[0]
+
+    if "S5 Release_version" in explog:
+        # version check for S5 XL
+        if explog['SystemType'] == 'S5XL':
+            if StrictVersion(version) >= StrictVersion('5.0'):
+                printResult(False, OK_STRING % version)
+            else:
+                printResult(True, ALERT_STRING % version)
+        # version check for S5
+        if explog['SystemType'] == 'S5':
+            if StrictVersion(version) >= StrictVersion('5.0.2'):
+                printResult(False, OK_STRING % version)
+            else:
+                printResult(True, ALERT_STRING % version)
+
+    # version check for PGM
+    elif "PGM SW Release" in explog:
+        if StrictVersion(version) >= StrictVersion('4.6'):
+            printResult(False, OK_STRING % version)
+        else:
+            printResult(True, ALERT_STRING % version)
+    # fallback version check
+    else:
+        if version:
+            printResult(False, OK_STRING % version)
+        else:
+            printResult(True, "Could not get a valid version to check against.")
+
+
