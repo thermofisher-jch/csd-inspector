2e724333fc0f2c1e9cfb77b4f22731014558cd5f
Brian Bourke-Martin <brian.bourke-martin@lifetech.com>
2016-03-08 10:16:24 -0500

    IO-138 * Updated the Chip Noise test to look at "Noise" or "ChipNoise" variables in the explog file.

--- lemontest/diagnostics/Chip_Noise/main.py
+++ lemontest/diagnostics/Chip_Noise/main.py
@@ -1,80 +1,39 @@
 #!/usr/bin/env python
 
 import sys
-import os
 from lemontest.diagnostics.common.inspector_utils import *
 
-
-def proton_correlated_noise(chip_noise_info):
-    noise_info = dict(x.split(":", 1) for x in chip_noise_info.split(" "))
-    return float(noise_info['Cor'])
-
-# the thresholds for warning and alerts (-1 for invalid warning)
-thresholds = {
-        "314": (-1.0, 6.0),
-        "316": (-1.0, 8.0),
-        "318": (-1.0, 10.0),
-        "P1": (-1.0, 170),
-        "520": (-1.0, 140),
-        "530": (-1.0, 140),
-        "540": (-1.0, 170),
-        "900": (1.0, 2.0)
+try:
+    thresholds = {
+        "314": 6.0,
+        "316": 8.0,
+        "318": 10.0,
+        "P1": 170,
+        "520": 140,
+        "530": 140,
+        "540": 170,
     }
 
-try:
     archive_path, output_path = sys.argv[1:3]
     data = read_explog(archive_path)
+    chip_type = get_chip_type_from_exp_log(data)
 
-    if "ChipType" not in data:
-        raise Exception("ChipType missing from explog_final.txt")
-
-    chip_type = data["ChipType"][:3]
-    if chip_type != '900' and "Noise_90pct" not in data:
-        raise Exception("Noise_90pct missing from explog_final.txt")
-    elif chip_type == '900':
-        if "ChipNoiseInfo" not in data:
-            raise Exception("ChipNoiseInfo missing from explog_final.txt")
-        else:
-            try:
-                info = data.get("ChipNoiseInfo", "")
-                noise = proton_correlated_noise(info)
-            except (ValueError, KeyError) as err:
-                raise Exception("Correlated chip noise is missing from explog_final.txt")
-
-    chipTypeString = data["ChipType"]
-    chip_type = data["ChipType"][:min(3, len(chipTypeString))]
-
+    # check to see if the we have a lookup value for the chip type
     if chip_type not in thresholds:
         raise Exception("No known thresholds for chip type " + chip_type)
 
-    low, high = thresholds[chip_type]
-    if "ChipNoiseInfo" in data:
-        noise = proton_correlated_noise(data["ChipNoiseInfo"])
-        if noise > high:
-            print("Alert")
-            print(40)
-            print("Chip noise {:.2f} is too high for chip type '{}'.".format(noise, chipTypeString))
-        elif noise > low:
-            print("Warning")
-            print(30)
-            print("Chip noise {:.2f} is a little high for chip type '{}'.".format(noise, chipTypeString))
-        else:
-            print("OK")
-            print(10)
-            print("Chip noise {:.2f} is low enough for chip type '{}'.".format(noise, chipTypeString))
-    elif "Noise_90pct" in data:
-        noise = float(data["Noise_90pct"])
-        if noise > high:
-            print("Alert")
-            print(40)
-            print("Chip noise {:.2f} is too high for a {}.".format(noise, chip_type))
-        else:
-            print("OK")
-            print(10)
-            print("Chip noise {:.2f} is low enough for a {}.".format(noise, chip_type))
+    # find the correct key for the noise value
+    noise = data.get('ChipNoise', None) or data.get('Noise', None)
+    if not noise:
+        raise Exception("The noise value could not be found in the log.")
+    noise = float(noise)
+
+    if noise > thresholds[chip_type]:
+        print_alert("Chip noise " + str(noise) + " is too high for chip type " + chip_type + ".")
+    else:
+        print_ok("Chip noise " + str(noise) + " is low enough for chip type " + chip_type + ".")
+
 except Exception as exc:
-    print("N/A")
-    print(0)
-    print(str(exc))
+    print_na(str(exc))
 
 
