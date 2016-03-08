312fe4cc1ec48f3070cf1b60f00e34ea8a26340c
Brian Bourke-Martin <brian.bourke-martin@lifetech.com>
2016-03-08 11:27:37 -0500

    IO-135 * Updated the chef version script to use common functionality * Now looks up for the first log in the gui logs directory and looks for 'chefPackageVer' in the json string output

--- lemontest/diagnostics/Chef_Version/main.py
+++ lemontest/diagnostics/Chef_Version/main.py
@@ -1,74 +1,47 @@
 #!/usr/bin/env python
 
-from xml.etree import ElementTree
-import xml.etree 
-import sys
+import fnmatch
+import json
 import os
-import os.path
-import re
+import sys
 from mako.template import Template
+from lemontest.diagnostics.common.inspector_utils import *
 
 
-if __name__ == "__main__":
+try:
     archive, output = sys.argv[1:3]
-    file_count = 0
-    files = []
-    output_name = ''
-    xml_path = ''
-    errors = []
     context = {
         "serial": "None",
         "versions": [],
     }
-    
-    for path, dirs, names in os.walk(archive):
-        if "test_results" not in path:
-            for name in names:
-                if "logs.tar" not in name:
-                    rel_dir = os.path.relpath(path, archive)
-                    rel = os.path.join(rel_dir, name)
-                    full = os.path.join(path, name)
-                    files.append(rel)
-                    file_count += 1
-                    if rel.startswith("var/log/IonChef/RunLog/") and rel.endswith(".xml"):
-                        xml_path = full
 
-    if xml_path:
-        # groom the xml of known error conditions
-        xml = ''
-        with open(xml_path, 'r') as xml_file:
-            xml = xml_file.read()
+    matches = list()
+    for root, dirnames, filenames in os.walk(os.path.join(archive, 'var', 'log', 'IonChef', 'ICS')):
+        for filename in fnmatch.filter(filenames, 'gui-*.log'):
+            matches.append(os.path.join(root, filename))
 
-        xml = re.sub('< *', '<', xml)
-        xml = re.sub('</ *', '</', xml)
-        xml = re.sub('> *', '>', xml)
+    root = get_xml_from_run_log(archive)
+    name_tag = root.find("Versions/is")
+    is_name = name_tag.text
+    name_tag = root.find("Versions/scripts")
+    scripts_name = name_tag.text
+    context['versions'] = [(t.tag, t.text) for t in root.findall("Versions/*")]
+    context['serial'] = root.find("Instrument/serial").text
 
-        try:
-            root = ElementTree.fromstring(xml)
-        except Exception as err:
-            errors.append("Error reading run log: " + str(err))
-        else:
-            name_tag = root.find("Versions/is")
-            is_name = name_tag.text
-            name_tag = root.find("Versions/scripts")
-            scripts_name = name_tag.text
-            output_name = "is: {} scripts: {}".format(is_name, scripts_name)
-            context['versions'] = [(t.tag, t.text) for t in root.findall("Versions/*")]
-            context['serial'] = root.find("Instrument/serial").text
+    summary = "Could not find any gui log to look up the chef version."
+    if len(matches) > 0:
+        for line in open(matches[0]):
+            if 'chefPackageVer' in line:
+                # we expect this to be a json output and will split on it primary delimiter
+                json_text = '{' +  line.split('{', 1)[1]
+                info = json.loads(json_text)
+                summary = info['chefPackageVer']
 
-        summary = output_name
-        print("Info")
-        print("20")
-        print(summary)
-        
-        template = Template(filename="results.mako")
-        result = template.render(**context)
-        with open(os.path.join(output, "results.html"), 'w') as out:
-            out.write(result.encode("UTF-8"))
+    print_info(summary)
+    template = Template(filename="results.mako")
+    result = template.render(**context)
+    with open(os.path.join(output, "results.html"), 'w') as out:
+        out.write(result.encode("UTF-8"))
 
-    else:
-        summary = "No Run Log."
-        print("N/A")
-        print("0")
-        print(summary)
-
+except Exception as exc:
+    print_na(str(exc))
