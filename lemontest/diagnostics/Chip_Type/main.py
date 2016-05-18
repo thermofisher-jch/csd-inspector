3f7df02b17fcd9932a3657333e2772a132e0d344
Brian Bourke-Martin <brian.bourke-martin@lifetech.com>
2016-05-18 15:11:48 -0400

    IO-151 * Added chip efuse reporting to chip type.

--- lemontest/diagnostics/Chip_Type/main.py
+++ lemontest/diagnostics/Chip_Type/main.py
@@ -8,7 +8,16 @@ try:
     # get the path to the log file
     archive_path, output_path = sys.argv[1:3]
     data = read_explog(archive_path)
-    print_info(get_chip_type_from_exp_log(data))
+    chip_type = get_chip_type_from_exp_log(data)
+
+    # get the chip efuse stuff
+    if 'Chip Efuse' in data:
+        chip_efuse = data['Chip Efuse'].split(',')
+        l = chip_efuse[0].split(':')[1]
+        w = chip_efuse[1].split(':')[1]
+        chip_type += " | " + l + "W" + w
+
+    print_info(chip_type)
 
 except Exception as exc:
     print_na(str(exc))
