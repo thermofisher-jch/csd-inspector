7f70a009df4e80857458c37af2e16f785ae108b2
Brian Bourke-Martin <brian.bourke-martin@lifetech.com>
2016-05-19 15:04:48 -0400

    IO-126 * Updated system type for PGM machines * Sequencing details will now gracefully handle no ion_params_00.json present.

--- lemontest/diagnostics/Sequencing_Details/main.py
+++ lemontest/diagnostics/Sequencing_Details/main.py
@@ -9,10 +9,12 @@ from lemontest.diagnostics.common.inspector_utils import *
 try:
     archive_path, output_path = sys.argv[1:3]
 
+    params_path = os.path.join(archive_path, 'ion_params_00.json')
     # read the ion params file
     params = dict()
-    with open(os.path.join(archive_path, 'ion_params_00.json')) as params_file:
-        params = json.load(params_file)
+    if os.path.exists(params_path):
+        with open(params_path) as params_file:
+            params = json.load(params_file)
 
     # check if the key is present in the dictionary
     template_kit_name = "Unknown Templating Kit"
@@ -22,7 +24,13 @@ try:
     # get the sequencing kit description from the exp log
     exp_log = read_explog(archive_path)
     inspector_seq_kit = exp_log.get("SeqKitDesc", None) or exp_log.get("SeqKitPlanDesc", None) or "Unknown Sequencing Kit"
-    system_type = exp_log.get("SystemType", None) or "Unknown System Type"
+
+    # get the system type
+    system_type = "Unknown System Type"
+    if "SystemType" in exp_log:
+        system_type = exp_log.get("SystemType")
+    elif "PGM HW" in exp_log:
+        system_type = "PGM" + exp_log.get("PGM HW")
 
     # print the details
     print_info(" | ".join([system_type, inspector_seq_kit, template_kit_name]))
