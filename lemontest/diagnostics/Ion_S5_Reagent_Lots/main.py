ce268a0102b15b3185e93bbfefc4d98b894537bb
Brian Bourke-Martin <brian.bourke-martin@thermofisher.com>
2016-12-02 16:06:27 -0500

    IO-202 * Updated the Ion S5 Reagent Lots tests to look for a list of keywords for sequencing and wash

--- lemontest/diagnostics/Ion_S5_Reagent_Lots/main.py
+++ lemontest/diagnostics/Ion_S5_Reagent_Lots/main.py
@@ -5,22 +5,24 @@ import sys
 from lemontest.diagnostics.common.inspector_utils import *
 
 
-def get_lot_from_lines(log_lines, lot_type):
+def get_lot_from_lines(log_lines, lot_types):
     """
     Helper method to grab the lot number from the lines variable
     :param log_lines: The lines in the log file
-    :param lot_type: The specific type of lot looking for
+    :param lot_types: The specific type of lot looking for
     :return: The string value of the lot type
     """
     for lineIndex in range(len(log_lines)):
         line = log_lines[lineIndex].strip()
 
-        if line.startswith('productDesc') and lot_type in line:
-            lot_line = log_lines[lineIndex + 2]
-            key_value = lot_line.split(":")
-            if key_value[0] != "lotNumber":
-                raise Exception("Did not find a lot number in expected location")
-            return "<b>" + key_value[1].strip() + "</b>"
+        if line.startswith('productDesc'):
+            for lot_type in lot_types:
+                if lot_type in line:
+                    lot_line = log_lines[lineIndex + 2]
+                    key_value = lot_line.split(":")
+                    if key_value[0] != "lotNumber":
+                        raise Exception("Did not find a lot number in expected location")
+                    return "<b>" + key_value[1].strip() + "</b>"
     return '<b>Unknown</b>'
 
 archive, output, archive_type = sys.argv[1:4]
@@ -28,17 +30,14 @@ try:
     data = read_explog(archive)
     check_supported(data)
 
-    # get the archive and output from argument list
-    path = os.path.join(archive, "InitLog.txt")
-
     # read all the lines into an array
-    with open(path) as f:
+    with open(os.path.join(archive, "InitLog.txt")) as f:
         lines = f.readlines()
 
     # construct the html response message
-    cleaningLot = "Cleaning " + get_lot_from_lines(lines, 'Ion S5 Cleaning Solution')
-    sequencingLot = "Sequencing " + get_lot_from_lines(lines, 'Ion S5 Sequencing Reagent')
-    washLot = "Wash " + get_lot_from_lines(lines, 'Ion S5 Wash Solution')
+    cleaningLot = "Cleaning " + get_lot_from_lines(lines, ['Ion S5 Cleaning Solution'])
+    sequencingLot = "Sequencing " + get_lot_from_lines(lines, ['Ion S5 Sequencing Reagent', 'Ion S5 ExT Sequencing Reagent'])
+    washLot = "Wash " + get_lot_from_lines(lines, ['Ion S5 Wash Solution', 'Ion S5 ExT Wash Solution'])
 
     print_info(" | ".join([cleaningLot, sequencingLot, washLot]))
 except Exception as exc:
