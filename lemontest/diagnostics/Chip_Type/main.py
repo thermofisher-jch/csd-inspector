db0719f18de15d6de49c1a0d68ba036dde06312d
Alex Roemer <Alexander.Roemer@lifetech.com>
2016-12-01 22:14:43 -0500

    IO-194 * More robust refuse line parsing fixed 520 issues

--- lemontest/diagnostics/Chip_Type/main.py
+++ lemontest/diagnostics/Chip_Type/main.py
@@ -3,23 +3,29 @@
 import sys
 from lemontest.diagnostics.common.inspector_utils import *
 
+archive_path, output_path = sys.argv[1:3]
 
 try:
     # get the path to the log file
-    archive_path, output_path = sys.argv[1:3]
     data = read_explog(archive_path)
     chip_type = get_chip_type_from_exp_log(data)
 
-    # get the chip efuse stuff
-    if 'Chip Efuse' in data:
-        chip_efuse = data['Chip Efuse'].split(',')
-        l = chip_efuse[0].split(':')[1]
-        w = chip_efuse[1].split(':')[1]
-        chip_type += " | Lot " + l + "W" + w
+    # parse efuse line
+    efuse_dict = {}
+    for pair in data.get('Chip Efuse', '').split(','):
+        if ":" in pair:
+            key, value = pair.split(":")
+            efuse_dict[key.strip()] = value.strip()
+
+    if "L" in efuse_dict:
+        chip_type += " | Lot " + efuse_dict["L"]
+
+    if "W" in efuse_dict:
+        chip_type += " | W " + efuse_dict["W"]
 
     print_info(chip_type)
 
 except Exception as exc:
-    print_na(str(exc))
+    handle_exception(exc, output_path)
 
 
