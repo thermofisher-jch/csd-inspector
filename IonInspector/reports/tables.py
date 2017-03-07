17db3efc455ffc5334794a50a1efef01ae244986
Alex Roemer <Alexander.Roemer@lifetech.com>
2017-03-07 16:05:11 -0500

    * Cleanup alignment on report table

--- IonInspector/reports/tables.py
+++ IonInspector/reports/tables.py
@@ -1,4 +1,5 @@
 from django.contrib.humanize.templatetags.humanize import naturaltime
+from django.utils.safestring import mark_safe
 import django_tables2 as tables
 from django_tables2.utils import A
 from reports.models import Archive
@@ -9,16 +10,22 @@ class ArchiveTable(tables.Table):
     Table for rendering archives.
     """
 
-    id = tables.Column(verbose_name='ID')
-    time = tables.Column(verbose_name='Date')
-    identifier = tables.LinkColumn('reports.views.report', args=[A('pk')], orderable=True, empty_values=(), verbose_name='Label', accessor='identifier')
-    site = tables.Column(verbose_name='Site')
-    submitter_name = tables.Column(verbose_name='Submitter')
-    archive_type = tables.Column(verbose_name='Type')
-    taser_ticket_number = tables.Column(verbose_name="TASER")
+    id = tables.Column(verbose_name='ID', attrs={'th': {'style': 'width: 60px'}})
+    time = tables.Column(verbose_name='Date', attrs={'th': {'style': 'width: 150px'}})
+    identifier = tables.LinkColumn('reports.views.report', args=[A('pk')], orderable=True, empty_values=(), verbose_name='Label', accessor='identifier', attrs={'th': {'style': 'width: 30%'}})
+    site = tables.Column(verbose_name='Site', attrs={'th': {'style': 'width: 20%'}})
+    submitter_name = tables.Column(verbose_name='Submitter', attrs={'th': {'style': 'width: 20%'}})
+    archive_type = tables.Column(verbose_name='Type', attrs={'th': {'style': 'width: 100px'}})
+    taser_ticket_number = tables.Column(verbose_name="TASER", attrs={'th': {'style': 'width: 80px'}})
 
     def render_time(self, value):
         return naturaltime(value)
+
+    def render_taser_ticket_number(self, value):
+        if value:
+            return mark_safe(
+                "<a href='https://jira.amer.thermo.com/browse/FST-%i' target='_blank'>TASER: %i</a>" % (value, value))
+        return ""
 
     class Meta:
         model = Archive
