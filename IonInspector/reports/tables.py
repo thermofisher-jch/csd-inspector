4af467dc0ce3e29d677af5f471b5d543daca8951
Alex Roemer <Alexander.Roemer@lifetech.com>
2017-03-08 00:12:07 -0500

    IO-219 * Restored reports table links for right click * Removed unused css

--- IonInspector/reports/tables.py
+++ IonInspector/reports/tables.py
@@ -3,6 +3,7 @@ from django.utils.safestring import mark_safe
 import django_tables2 as tables
 from django_tables2.utils import A
 from reports.models import Archive
+from django.core.urlresolvers import reverse
 
 
 class ArchiveTable(tables.Table):
@@ -10,22 +11,44 @@ class ArchiveTable(tables.Table):
     Table for rendering archives.
     """
 
-    id = tables.Column(verbose_name='ID', attrs={'th': {'style': 'width: 60px'}})
-    time = tables.Column(verbose_name='Date', attrs={'th': {'style': 'width: 150px'}})
-    identifier = tables.LinkColumn('reports.views.report', args=[A('pk')], orderable=True, empty_values=(), verbose_name='Label', accessor='identifier', attrs={'th': {'style': 'width: 30%'}})
-    site = tables.Column(verbose_name='Site', attrs={'th': {'style': 'width: 20%'}})
-    submitter_name = tables.Column(verbose_name='Submitter', attrs={'th': {'style': 'width: 20%'}})
-    archive_type = tables.Column(verbose_name='Type', attrs={'th': {'style': 'width: 100px'}})
-    taser_ticket_number = tables.Column(verbose_name="TASER", attrs={'th': {'style': 'width: 80px'}})
+    id = tables.Column(verbose_name='ID', attrs={'th': {'style': 'width: 60px'}}, empty_values=())
+    time = tables.Column(verbose_name='Date', attrs={'th': {'style': 'width: 150px'}}, empty_values=())
+    identifier = tables.Column(verbose_name='Label', orderable=True, empty_values=(), accessor='identifier',
+                               attrs={'th': {'style': 'width: 30%'}})
+    site = tables.Column(verbose_name='Site', attrs={'th': {'style': 'width: 20%'}}, empty_values=())
+    submitter_name = tables.Column(verbose_name='Submitter', attrs={'th': {'style': 'width: 20%'}}, empty_values=())
+    archive_type = tables.Column(verbose_name='Type', attrs={'th': {'style': 'width: 100px'}}, empty_values=())
+    taser_ticket_number = tables.Column(verbose_name="TASER", attrs={'th': {'style': 'width: 80px'}}, empty_values=())
 
-    def render_time(self, value):
-        return naturaltime(value)
+    def render_id(self, value, record):
+        return mark_safe(
+            "<a href='%s' class='no-underline' target='_blank'>%s</a>" % (reverse('report', args=[record.id]), value))
 
-    def render_taser_ticket_number(self, value):
+    def render_time(self, value, record):
+        return mark_safe("<a href='%s' class='no-underline' target='_blank'>%s</a>" % (
+        reverse('report', args=[record.id]), naturaltime(value)))
+
+    def render_identifier(self, value, record):
+        return mark_safe(
+            "<a href='%s' class='no-underline' target='_blank'>%s</a>" % (reverse('report', args=[record.id]), value))
+
+    def render_site(self, value, record):
+        return mark_safe(
+            "<a href='%s' class='no-underline' target='_blank'>%s</a>" % (reverse('report', args=[record.id]), value))
+
+    def render_submitter_name(self, value, record):
+        return mark_safe(
+            "<a href='%s' class='no-underline' target='_blank'>%s</a>" % (reverse('report', args=[record.id]), value))
+
+    def render_archive_type(self, value, record):
+        return mark_safe(
+            "<a href='%s' class='no-underline' target='_blank'>%s</a>" % (reverse('report', args=[record.id]), value))
+
+    def render_taser_ticket_number(self, value, record):
         if value:
             return mark_safe(
                 "<a href='https://jira.amer.thermo.com/browse/FST-%i' target='_blank'>TASER: %i</a>" % (value, value))
-        return ""
+        return mark_safe("<a href='%s' class='no-underline' target='_blank'></a>" % reverse('report', args=[record.id]))
 
     class Meta:
         model = Archive
