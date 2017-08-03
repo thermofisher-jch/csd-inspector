57b381e6e64e2df3f6f85b2b0efa899ad618cf9d
Alex Roemer <Alexander.Roemer@thermofisher.com>
2017-08-03 11:34:42 -0400

    Updated fabfile for deploy

--- fabfile.py
+++ fabfile.py
@@ -39,8 +39,9 @@ def deploy(tag=None):
         if raw_input("This looks like a deploy to production! Enter PRODUCTION to continue: ") != "PRODUCTION":
             exit()
     with cd(HOST_DATA_DIR + "/inspector"):
+        run("git fetch")
         run("git checkout %s" % tag)
-        run("git pull")
+        run("git fetch")
         run("docker-compose -f docker-compose.yml -f docker-compose.prod.yml build")
         run("docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d")
         upload_template("./conf/nginx.conf", "/etc/nginx/sites-enabled/inspector.conf", {
