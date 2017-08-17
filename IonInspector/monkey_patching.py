1a332d696ce30e8fa84d6662ec0515991fb76bf9
Alex Roemer <Alexander.Roemer@thermofisher.com>
2017-08-17 12:45:47 -0400

    IO-243 * Patch the django directory_index method to sort files alpha

---
+++ IonInspector/monkey_patching.py
@@ -0,0 +1,30 @@
+import os
+
+from django.http import HttpResponse
+from django.template import loader, Template, Context, TemplateDoesNotExist
+import django.views.static
+
+
+# Need alphabetical order on dir listings
+# django/views/static.py#L102
+
+def directory_index(path, fullpath):
+    try:
+        t = loader.select_template(['static/directory_index.html',
+                                    'static/directory_index'])
+    except TemplateDoesNotExist:
+        t = Template(django.views.static.DEFAULT_DIRECTORY_INDEX_TEMPLATE, name='Default directory index template')
+    files = []
+    for f in sorted(os.listdir(fullpath), key=lambda s: s.lower()):
+        if not f.startswith('.'):
+            if os.path.isdir(os.path.join(fullpath, f)):
+                f += '/'
+            files.append(f)
+    c = Context({
+        'directory': path + '/',
+        'file_list': files,
+    })
+    return HttpResponse(t.render(c))
+
+
+django.views.static.directory_index = directory_index
