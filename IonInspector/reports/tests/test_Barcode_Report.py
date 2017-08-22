7e8c0ef7d48d94d0cda13a498fd4f5efa3d506ba
Alex Roemer <Alexander.Roemer@thermofisher.com>
2017-08-22 16:18:52 -0400

    IO-217 * Added toggle on barcode report

---
+++ IonInspector/reports/tests/test_Barcode_Report.py
@@ -0,0 +1,70 @@
+from xml.etree import ElementTree
+
+from django.test import SimpleTestCase
+
+from lemontest.diagnostics.Barcode_Report.main import get_read_group_file_prefixes, get_read_groups
+
+
+class BarcodeReportTestCase(SimpleTestCase):
+    datasets_basecaller_object = {
+        "datasets": [
+            {
+                "basecaller_bam": "nomatch_rawlib.basecaller.bam",
+                "dataset_name": "none/No_barcode_match",
+                "file_prefix": "nomatch_rawlib",
+                "read_count": 160472,
+                "read_groups": ["3CFN9.nomatch"]
+            },
+            {
+                "basecaller_bam": "IonXpress_001_rawlib.basecaller.bam",
+                "dataset_name": "none/IonXpress_001",
+                "file_prefix": "IonXpress_001_rawlib",
+                "read_count": 0,
+                "read_groups": ["3CFN9.IonXpress_001"]
+            }],
+        "read_groups": {
+            "3CFN9.IonXpress_001": {
+                "Q20_bases": 0,
+                "barcode_adapter": "GAT",
+                "barcode_adapter_filtered": 6,
+                "barcode_bias": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                "barcode_distance_hist": [0, 0, 0, 0, 0],
+                "barcode_errors_hist": [0, 0, 0],
+                "barcode_match_filtered": 0,
+                "barcode_name": "IonXpress_001",
+                "barcode_sequence": "CTAAGGTAAC",
+                "description": "sample24repeatchiplot1OT3",
+                "filtered": True,
+                "index": 1,
+                "platform_unit": "proton/P2.2.2/IonXpress_001",
+                "read_count": 0,
+                "reference": "hg19",
+                "sample": "none",
+                "total_bases": 0
+            },
+            "3CFN9.nomatch": {
+                "Q20_bases": 3366033,
+                "description": "sample24repeatchiplot1OT3",
+                "index": 0,
+                "platform_unit": "proton/P2.2.2/nomatch",
+                "read_count": 160472,
+                "reference": "",
+                "sample": "none",
+                "total_bases": 27715310
+            }
+        }
+    }
+
+    def test_get_read_group_file_prefixes(self):
+        prefixes = get_read_group_file_prefixes(self.datasets_basecaller_object)
+        self.assertEqual(prefixes["3CFN9.nomatch"], "nomatch_rawlib")
+        self.assertEqual(prefixes["3CFN9.IonXpress_001"], "IonXpress_001_rawlib")
+
+    def test_get_read_groups(self):
+        groups = get_read_groups(self.datasets_basecaller_object)
+        self.assertEqual(groups[0],
+                         {"group": "3CFN9.nomatch", "name": "nomatch", "read_count": 160472, "index": 0,
+                          "filtered": False})
+        self.assertEqual(groups[1],
+                         {"group": "3CFN9.IonXpress_001", "name": "IonXpress_001", "read_count": 0, "index": 1,
+                          "filtered": True})
--- lemontest/diagnostics/Barcode_Report/main.py
+++ lemontest/diagnostics/Barcode_Report/main.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python
 
-import glob
 import os
 import shutil
 import sys
+import json
 
 from lemontest.diagnostics.common.inspector_utils import write_results_from_template, print_info
 
 
+def get_read_group_file_prefixes(datasets_basecaller_object):
+    file_prefixes = {}
+    for item in datasets_basecaller_object["datasets"]:
+        for group in item["read_groups"]:
+            file_prefixes[group] = item["file_prefix"]
+    return file_prefixes
+
+
+def get_read_groups(datasets_basecaller_object):
+    groups = []
+    for key, value in datasets_basecaller_object["read_groups"].iteritems():
+        groups.append({
+            "filtered": value.get("filtered", False),
+            "name": value.get("barcode_name", "nomatch"),
+            "read_count": value.get("read_count", 0),
+            "index": value.get("index", -1),
+            "group": key
+        })
+    return sorted(groups, key=lambda k: k['index'])
+
+
 def get_read_length_histograms(archive_path, output_path):
+    with open(os.path.join(archive_path, "basecaller_results/datasets_basecaller.json")) as datasets_file:
+        datasets_object = json.load(datasets_file)
+
+    prefixes = get_read_group_file_prefixes(datasets_object)
+    groups = get_read_groups(datasets_object)
+
     histograms = []
-    for image_path in glob.glob(os.path.join(archive_path, "basecaller_results/*sparkline.png")):
-        image_filename = os.path.basename(image_path)
-        # Copy to test dir
-        shutil.copyfile(image_path, os.path.join(output_path, image_filename))
-        # Generate name and path
-        name = image_filename.replace(".sparkline.png", "")
-        histograms.append((name, image_filename))
+    for group in groups:
+        prefix = prefixes[group["group"]]
+        src_image_path = os.path.join(archive_path, "basecaller_results/{}.sparkline.png".format(prefix))
+        dst_image_path = os.path.join(output_path, "{}.sparkline.png".format(prefix))
+        if os.path.exists(src_image_path):
+            # Copy to test dir
+            shutil.copyfile(src_image_path, dst_image_path)
+            histograms.append([group, os.path.basename(dst_image_path)])
+        else:
+            histograms.append([group, None])
     return histograms
 
 
