254a16d753858bf61c0ef3dd8d4e8e2450993c5d
Alex Roemer <Alexander.Roemer@thermofisher.com>
2017-08-24 15:18:52 -0400

    * Bump version to 1.1.10

--- IonInspector/settings.py
+++ IonInspector/settings.py
@@ -141,7 +141,7 @@ CELERY_RESULT_BACKEND = 'amqp'
 CELERY_RESULT_SERIALIZER = 'pickle'
 SITE_ROOT = os.path.dirname(os.path.dirname(__file__))
 
-VERSION = '1.1.9'
+VERSION = '1.1.10'
 
 try:
     from local_version import *
