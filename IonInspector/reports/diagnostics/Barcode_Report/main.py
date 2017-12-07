a6a30f03da783eedd05d3e6ee0a1c6535b2ac801
Brian Bourke-Martin <brian.bourke-martin@thermofisher.com>
2017-12-07 15:18:37 -0500

    IO-282 * Added numpy to the docker base image * Updated barcode report to generate the requested statistics. * Updated the report to have a statistics tables

--- IonInspector/reports/diagnostics/Barcode_Report/main.py
+++ IonInspector/reports/diagnostics/Barcode_Report/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import os
+import numpy
 import shutil
 import sys
 import json
 
-from IonInspector.reports.diagnostics.common.inspector_utils import write_results_from_template, print_info
+from IonInspector.reports.diagnostics.common.inspector_utils import *
 
 
 def get_read_group_file_prefixes(datasets_basecaller_object):
@@ -29,31 +30,38 @@ def get_read_groups(datasets_basecaller_object):
     return sorted(groups, key=lambda k: k['index'])
 
 
-def get_read_length_histograms(archive_path, output_path):
-    with open(os.path.join(archive_path, "basecaller_results/datasets_basecaller.json")) as datasets_file:
-        datasets_object = json.load(datasets_file)
-
-    prefixes = get_read_group_file_prefixes(datasets_object)
-    groups = get_read_groups(datasets_object)
-
-    histograms = []
-    for group in groups:
-        prefix = prefixes[group["group"]]
-        src_image_path = os.path.join(archive_path, "basecaller_results/{}.sparkline.png".format(prefix))
-        dst_image_path = os.path.join(output_path, "{}.sparkline.png".format(prefix))
-        if os.path.exists(src_image_path):
-            # Copy to test dir
-            shutil.copyfile(src_image_path, dst_image_path)
-            histograms.append([group, os.path.basename(dst_image_path)])
-        else:
-            histograms.append([group, None])
-    return histograms
-
-
 def execute(archive_path, output_path, archive_type):
     try:
+        with open(os.path.join(archive_path, "basecaller_results/datasets_basecaller.json")) as datasets_file:
+            datasets_object = json.load(datasets_file)
+
+        prefixes = get_read_group_file_prefixes(datasets_object)
+        groups = get_read_groups(datasets_object)
+
+        # get all of the filtered data sets
+        filtered_data = [float(x['read_count']) for x in groups if not x['filtered'] and "nomatch" not in x['name']]
+        mean = numpy.mean(filtered_data)
+        min = numpy.min(filtered_data)
+        std = numpy.std(filtered_data)
+
+        histograms = []
+        for group in groups:
+            prefix = prefixes[group["group"]]
+            src_image_path = os.path.join(archive_path, "basecaller_results/{}.sparkline.png".format(prefix))
+            dst_image_path = os.path.join(output_path, "{}.sparkline.png".format(prefix))
+            if os.path.exists(src_image_path):
+                # Copy to test dir
+                shutil.copyfile(src_image_path, dst_image_path)
+                histograms.append([group, os.path.basename(dst_image_path)])
+            else:
+                histograms.append([group, None])
+
         write_results_from_template({
-            'histograms': get_read_length_histograms(archive_path, output_path)
+            'histograms': histograms,
+            'mean': mean,
+            'std': std,
+            'cv': (std / mean) * 100.0,
+            'min_percent': (min / mean) * 100.0,
         }, output_path)
         return print_info("See results for details.")
     except Exception as exc:
@@ -62,4 +70,4 @@ def execute(archive_path, output_path, archive_type):
 
 if __name__ == "__main__":
     archive_path, output_path, archive_type = sys.argv[1:4]
-    execute(archive_path, output_path, archive_type)
+    execute(archive_path=archive_path, output_path=output_path, archive_type=archive_type)
