7d3645527cb4d3cf42ee6c6f7b900da83fff16a9
Alex Roemer <Alexander.Roemer@thermofisher.com>
2018-02-19 15:21:13 -0500

    * Added --rm to backup job to stop processes from running forever

--- conf/backup_inspector_database.sh
+++ conf/backup_inspector_database.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /var/lib/inspector/inspector
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.jobs.yml run django python manage.py dumpdata reports | gzip > /mnt/raid/inspector/backups/reports_$(date +%s).json.gz
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.jobs.yml run --rm django python manage.py dumpdata reports | gzip > /mnt/raid/inspector/backups/reports_$(date +%s).json.gz
