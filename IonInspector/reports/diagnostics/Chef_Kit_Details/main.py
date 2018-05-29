ac6451858e7615e0730b3224147b2cae36fdd540
Alex Roemer <Alexander.Roemer@thermofisher.com>
2018-05-29 10:55:57 -0400

    IO-322 * Fixed balance chip display on Chef_Kit_Details

--- IonInspector/reports/diagnostics/Chef_Kit_Details/main.py
+++ IonInspector/reports/diagnostics/Chef_Kit_Details/main.py
@@ -46,13 +46,16 @@ def get_chip_names_from_element_tree(element_tree):
     * There may be two chips <chip> and <chip2>
     * For proton chips, the <chip> element only contains the major version number. <chipVersion>
       contains the minor version. So P1v3 would be <chip>1</chip> <chipVersion>3</chipVersion>
+    * Chip 500 is the chef balance chip
     """
     names = [None, None]
     for i, suffix in enumerate(["", "2"]):
         chip_element = element_tree.find("./RunInfo/chip" + suffix)
         chip_version_element = element_tree.find("./RunInfo/chipVersion" + suffix)
         if chip_element is not None:
-            if chip_element.text.isdigit() and int(chip_element.text) < 10:
+            if chip_element.text == "500":
+                names[i] = "Balance"
+            elif chip_element.text.isdigit() and int(chip_element.text) < 10:
                 if chip_version_element is not None:
                     names[i] = "P" + chip_element.text + "v" + chip_version_element.text
             else:
