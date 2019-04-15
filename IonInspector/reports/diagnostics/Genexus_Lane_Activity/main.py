760dcd92f3af1dcf4a0bb57f58193ed124a60413
Alexander Roemer <Alexander.Roemer@thermofisher.com>
2019-04-15 11:51:32 -0400

    * Add loading image to lane activity

--- IonInspector/reports/diagnostics/Genexus_Lane_Activity/main.py
+++ IonInspector/reports/diagnostics/Genexus_Lane_Activity/main.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 
+import os
+import shutil
+from collections import OrderedDict
+
 from IonInspector.reports.diagnostics.common.inspector_utils import (
     read_explog,
     print_info,
+    print_failed,
+    write_results_from_template,
 )
-from collections import OrderedDict
 
 
 def execute(archive_path, output_path, archive_type):
+    try:
+        shutil.copy(
+            os.path.join(
+                archive_path, "CSA/outputs/SigProcActor-00/Bead_density_1000.png"
+            ),
+            os.path.join(output_path, "Bead_density_1000.png"),
+        )
+    except IOError:
+        return print_failed("Could not find bead density image!")
+
     data = read_explog(archive_path)
     lanes = OrderedDict()
     lanes["1"] = (
@@ -27,7 +42,9 @@ def execute(archive_path, output_path, archive_type):
         data.get("LanesAssay4", "Unknown"),
     )
 
-    message = " | ".join(
-        ["L{} {}".format(k, v[1]) for k, v in lanes.items() if v[0]]
+    write_results_from_template(
+        {"lanes": lanes}, output_path, os.path.dirname(os.path.realpath(__file__))
     )
+
+    message = " | ".join(["L{} {}".format(k, v[1]) for k, v in lanes.items() if v[0]])
     return print_info(message)
