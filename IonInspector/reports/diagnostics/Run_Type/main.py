cfe1b46523037da071cdffa62ea349c07376d252
Alexander Roemer <Alexander.Roemer@thermofisher.com>
2019-07-16 12:32:35 -0400

    IO-356 * Add workflow to run summary

--- IonInspector/reports/diagnostics/Run_Type/main.py
+++ IonInspector/reports/diagnostics/Run_Type/main.py
@@ -6,7 +6,25 @@ from IonInspector.reports.diagnostics.common.inspector_utils import *
 def execute(archive_path, output_path, archive_type):
     explog = read_explog(archive_path)
     run_type = explog.get("RunType", "Unknown")
-    return print_info("Run Type: {}".format(run_type))
 
+    valkyrie_workflow_path = os.path.join(archive_path, "ValkyrieWorkflow") + "/"
 
+    files = [
+        "workflow_timing.png",
+        "VacuumLog.html",
+        "TubeBottomLog.csv",
+        "flow_spark.svg"
+    ]
 
+    if os.path.exists(valkyrie_workflow_path + "ValkyrieWorkflow_block.html"):
+
+        shutil.copy(valkyrie_workflow_path + "ValkyrieWorkflow_block.html",
+                    os.path.join(output_path, "results.html"))
+        for file in files:
+            try:
+                shutil.copy(valkyrie_workflow_path + file, os.path.join(output_path, file))
+            except IOError:
+                pass
+
+    message = "Run Type: {}".format(run_type)
+    return print_info(message)
