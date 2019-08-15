055bbd2d187855018a94a0159c43a95853ce62ca
Alexander Roemer <Alexander.Roemer@thermofisher.com>
2019-08-15 13:43:54 -0400

    IO-333 * Add reads per lane to activity

--- IonInspector/reports/diagnostics/Genexus_Lane_Activity/main.py
+++ IonInspector/reports/diagnostics/Genexus_Lane_Activity/main.py
@@ -6,11 +6,18 @@ from collections import OrderedDict
 from django.conf import settings
 
 from IonInspector.reports.diagnostics.common.inspector_utils import (
+    read_ionstats_basecaller_json,
     read_explog,
     print_info,
     print_failed,
     write_results_from_template,
 )
+
+
+def get_total_reads(archive_path, archive_type):
+    ionstats = read_ionstats_basecaller_json(archive_path, archive_type)
+    total_reads = ionstats["full"]["num_reads"]
+    return total_reads
 
 
 def execute(archive_path, output_path, archive_type):
@@ -32,6 +39,9 @@ def execute(archive_path, output_path, archive_type):
         data.get("LanesActive4", "no") == "yes",
         data.get("LanesAssay4", "Unknown"),
     )
+
+    total_reads = get_total_reads(archive_path, archive_type)
+    active_lanes = [active for active, assay in lanes.values()].count(True)
 
     # copy bead density
     bead_image_path = os.path.join(output_path, "Bead_density_1000.png")
@@ -61,12 +71,24 @@ def execute(archive_path, output_path, archive_type):
                         os.path.join(output_path, filename),
                     )
                 except IOError:
-                    return print_failed("Could not find lane diagnostics image '{}'!".format(filename))
+                    return print_failed(
+                        "Could not find lane diagnostics image '{}'!".format(filename)
+                    )
 
     # write template
     write_results_from_template(
-        {"lanes": lanes, "bead_image_url": bead_image_url}, output_path, os.path.dirname(os.path.realpath(__file__))
+        {
+            "lanes": lanes,
+            "bead_image_url": bead_image_url,
+            "total_reads": total_reads,
+            "active_lanes": active_lanes,
+            "average_reads_per_lane": total_reads / active_lanes
+        },
+        output_path,
+        os.path.dirname(os.path.realpath(__file__)),
     )
 
-    message = " <br> ".join(["<strong>L{}</strong> {}".format(k, v[1]) for k, v in lanes.items() if v[0]])
+    message = " <br> ".join(
+        ["<strong>L{}</strong> {}".format(k, v[1]) for k, v in lanes.items() if v[0]]
+    )
     return print_info(message)
