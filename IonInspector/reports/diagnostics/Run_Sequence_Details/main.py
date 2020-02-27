920468334b700b35fde3faf3db3372d54a5f5ad1
Ivan Pan <ivan.pan@thermofisher.com>
2020-02-27 09:07:19 -0800

    IO-388 update device name parsing for Valkyrie

--- IonInspector/reports/diagnostics/Run_Sequence_Details/main.py
+++ IonInspector/reports/diagnostics/Run_Sequence_Details/main.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
 import sys
+import os
+import json
 
 from dateutil.parser import parse
 
-from IonInspector.reports.diagnostics.common.inspector_utils import *
+from IonInspector.reports.diagnostics.common.inspector_utils import (
+    read_explog,
+    check_supported,
+    get_ts_version,
+    write_results_from_template,
+    handle_exception,
+)
 
 OK_STRING = "TS Version is acceptable at <strong>%s</strong>"
 ALERT_STRING = "Advise customer to upgrade their Torrent Server.  Their version is out-dated at <strong>%s</strong>"
@@ -12,11 +20,31 @@ ALERT_STRING = "Advise customer to upgrade their Torrent Server.  Their version 
 
 def parse_run_number_from_run_name(run_name, device_name):
     if run_name.startswith(device_name):
-        run_name = run_name[len(device_name):]
+        run_name = run_name[len(device_name) :]
     try:
         return run_name.split("-")[1]
     except Exception as e:
         return "Unknown"
+
+
+def get_device_name(explog, ion_param_exp):
+    if "DeviceName" in explog:
+        return explog.get("DeviceName")
+
+    if "pgmName" in ion_param:
+        return explog.get("pgmName")
+
+    return ""
+
+
+def get_system_type(explog, archive_type):
+    if archive_type == "PGM_RUN":
+        return "PGM %s" % explog.get("PGM HW", "")
+
+    if archive_type == "Valkyrie":
+        return explog.get("Platform", "Valkyrie")
+
+    return explog.get("SystemType", "")
 
 
 def execute(archive_path, output_path, archive_type):
@@ -28,8 +56,8 @@ def execute(archive_path, output_path, archive_type):
         check_supported(explog)
 
         ion_params_paths = [
-            os.path.join(archive_path, "CSA", 'ion_params_00.json'),
-            os.path.join(archive_path, 'ion_params_00.json')
+            os.path.join(archive_path, "CSA", "ion_params_00.json"),
+            os.path.join(archive_path, "ion_params_00.json"),
         ]
         for ion_params_path in ion_params_paths:
             if os.path.exists(ion_params_path):
@@ -43,43 +71,52 @@ def execute(archive_path, output_path, archive_type):
         version = get_ts_version(archive_path)
 
         # get the reagent and solution lots and experation dates
-        run_date = parse(explog.get('Start Time', 'Unknown'))
-        flows = explog.get('Flows', '')
-        serial_number = explog.get('Serial Number', 'Unknown')
-        system_type = 'PGM ' + explog.get('PGM HW', '') if 'PGM_Run' == archive_type else explog.get('SystemType', '')
+        run_date = parse(explog.get("Start Time", "Unknown"))
+        flows = explog.get("Flows", "")
+        serial_number = explog.get("Serial Number", "Unknown")
 
-        exp_json = ion_params.get('exp_json', dict())
+        system_type = get_system_type(explog, archive_type)
+
+        exp_json = ion_params.get("exp_json", dict())
         # Fix a valk bug where "exp_json" is sometime a json string instead of the object
         if isinstance(exp_json, basestring):
             exp_json = json.loads(exp_json)
 
-        device_name = exp_json.get('pgmName', "")
+        device_name = get_device_name(explog, exp_json)
 
-        run_number = str(exp_json.get('log', dict()).get('run_number', ""))
+        run_number = str(exp_json.get("log", dict()).get("run_number", ""))
         if not run_number:
             run_number = parse_run_number_from_run_name(
-                run_name=exp_json.get('log', dict()).get('runname', ""),
-                device_name=device_name
+                run_name=exp_json.get("log", dict()).get("runname", ""),
+                device_name=device_name,
             )
 
-        datetime_output_format = '%Y/%m/%d'
+        datetime_output_format = "%Y/%m/%d"
         template_context = {
-            'tss_version': version,
-            'device_name': device_name,
-            'run_number': run_number,
-            'run_date': run_date.strftime(datetime_output_format),
-            'flows': flows,
-            'serial_number': serial_number,
-            'system_type': system_type,
+            "tss_version": version,
+            "device_name": device_name,
+            "run_number": run_number,
+            "run_date": run_date.strftime(datetime_output_format),
+            "flows": flows,
+            "serial_number": serial_number,
+            "system_type": system_type,
         }
-        write_results_from_template(template_context, output_path, os.path.dirname(os.path.realpath(__file__)))
+        write_results_from_template(
+            template_context, output_path, os.path.dirname(os.path.realpath(__file__))
+        )
 
-        return print_info(" | ".join([
-            "TS " + template_context["tss_version"],
-            template_context["device_name"] + " - " + template_context["run_number"],
-            template_context["run_date"],
-            template_context["flows"] + " flows"
-        ]))
+        return print_info(
+            " | ".join(
+                [
+                    "TS " + template_context["tss_version"],
+                    template_context["device_name"]
+                    + " - "
+                    + template_context["run_number"],
+                    template_context["run_date"],
+                    template_context["flows"] + " flows",
+                ]
+            )
+        )
     except Exception as exc:
         return handle_exception(exc, output_path)
 
