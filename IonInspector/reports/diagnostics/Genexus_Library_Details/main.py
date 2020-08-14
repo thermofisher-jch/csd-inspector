4fe22e3e0706679dbb2d950e8664bd7d590bf852
Ivan Pan <ivan.pan@thermofisher.com>
2020-08-13 22:32:02 -0700

    IO-389 add back Anneal/Extend and Library Amp Cycle
    - display column if these values are found
    - add Sample input in ng

--- IonInspector/reports/diagnostics/Genexus_Library_Details/main.py
+++ IonInspector/reports/diagnostics/Genexus_Library_Details/main.py
@@ -10,17 +10,38 @@ from IonInspector.reports.diagnostics.common.inspector_utils import (
 AMPLIFICATION_IDX = 1
 CYCLE_IDX = 2
 
+
 def execute(archive_path, output_path, archive_type):
     with open(os.path.join(archive_path, "CSA/planned_run.json")) as fp:
         deck_configs = json.load(fp)["object"]["deckConfig"]
 
+    # default to false
+    column_opts = {
+        "annealExtendTime": False,
+        "numLibraryAmpCycles": False,
+    }
+
     sample_maps = []
     for config in deck_configs:
         if "sampleBarcodeMapping" in config:
+            for sample in config["sampleBarcodeMapping"]:
+                sample["sampleInput"] = (
+                        float(sample["sampleVolume"])
+                        * float(sample["sampleConcentration"])
+                        / float(sample["sampleDilutionFactor"])
+                )
             sample_maps.extend(config["sampleBarcodeMapping"])
 
         if "pcrProfile" not in config:
             continue
+
+        # once found, enable
+        if "annealExtendTime" in config["pcrProfile"]:
+            column_opts["annealExtendTime"] = True
+
+        # once found, enable
+        if "numLibraryAmpCycles" in config["pcrProfile"]:
+            column_opts["numLibraryAmpCycles"] = True
 
         pcrProfile = config["pcrProfile"]
         if "numTargetAmpCycles" not in pcrProfile:
@@ -29,13 +50,18 @@ def execute(archive_path, output_path, archive_type):
         str_literal = pcrProfile.get("amplifyTargetPCRProfile", None)
         if str_literal:
             amplifyTargetPRCProfile = ast.literal_eval(str_literal)
-            pcrProfile["numTargetAmpCycles"] = str(amplifyTargetPRCProfile[AMPLIFICATION_IDX][CYCLE_IDX])
+            pcrProfile["numTargetAmpCycles"] = str(
+                amplifyTargetPRCProfile[AMPLIFICATION_IDX][CYCLE_IDX]
+            )
 
     write_results_from_template(
-        {"deck_configs": deck_configs, "sample_maps": sample_maps},
+        {
+            "deck_configs": deck_configs,
+            "sample_maps": sample_maps,
+            "column_opts": column_opts,
+        },
         output_path,
         os.path.dirname(os.path.realpath(__file__)),
     )
 
     return print_info("See results for details.")
-    # return print_info(str(deck_configs[-1]["pcrProfile"]))
