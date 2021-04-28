b00f091201d59cc0755610bc47045469489a8d5d
Heinnickel, John C <john.heinnickel@thermofisher.com>
2021-04-28 05:57:52 -0700

    This script never intended for commit.  Was a one-time use development tool.

--- prod_run_volume_init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# export NGINX_TEMP_ROOT=/mnt/raid_old/inspector/nginxTemp
-export NGINX_TEMP_ROOT=/var/lib/inspector/nginxTemp
-export MEDIA_ROOT=/mnt/raid/inspector/media
-./init_volume_paths.sh
