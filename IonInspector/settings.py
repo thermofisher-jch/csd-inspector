40590a4c2d2b2fd5dea574d50a4ca0d029ac62c4
john-heinnickel <78825323+john-heinnickel@users.noreply.github.com>
2021-09-15 10:01:17 -0700

    Update RELEASE variable for 1.8.0-rc.3 (#15)
    * Update RELEASE variable for 1.8.0-rc.3
    
    * IO-480 Applying Black formatting to avoid complex merge conflict

--- IonInspector/settings.py
+++ IonInspector/settings.py
@@ -18,56 +18,60 @@ BASE_DIR = os.path.dirname(__file__)
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', '@0gi@=8!#@w!g8d6vibm9+576!rhe949a#w)unk+3s5f4fe0x2')
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "@0gi@=8!#@w!g8d6vibm9+576!rhe949a#w)unk+3s5f4fe0x2"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '' if DEBUG else 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS", "" if DEBUG else "localhost,127.0.0.1"
+).split(",")
 ALLOWED_HOSTS = [] if not any(ALLOWED_HOSTS) else ALLOWED_HOSTS
 
 # Application definition
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize',
-    'reports',
-    'crispy_forms',
-    'django_filters',
-    'django_tables2',
-    'tastypie',
-    'raven.contrib.django.raven_compat'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.humanize",
+    "reports",
+    "crispy_forms",
+    "django_filters",
+    "django_tables2",
+    "tastypie",
+    "raven.contrib.django.raven_compat",
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 )
 
-ROOT_URLCONF = 'IonInspector.urls'
+ROOT_URLCONF = "IonInspector.urls"
 
-WSGI_APPLICATION = 'IonInspector.wsgi.application'
+WSGI_APPLICATION = "IonInspector.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 # HOST Note!!! this is defined by the name of the docker container which is running postgres
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'IonInspector',
-        'USER': os.getenv('DATABASE_USERNAME', 'docker'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'docker'),
-        'HOST': 'postgres',
-        'PORT': ''
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "IonInspector",
+        "USER": os.getenv("DATABASE_USERNAME", "docker"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "docker"),
+        "HOST": "postgres",
+        "PORT": "",
     }
 }
 
@@ -77,20 +81,20 @@ DATABASES = {
 
 # Logging
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/inspector/django.log',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "/var/log/inspector/django.log",
         },
     },
-    'loggers': {
-        '': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': True,
+    "loggers": {
+        "": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True,
         },
     },
 }
@@ -98,9 +102,9 @@ LOGGING = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -111,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
@@ -119,39 +123,39 @@ STATICFILES_DIRS = [
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': True,
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
                 "IonInspector.reports.context_processors.version_number",
                 "IonInspector.reports.context_processors.use_datatables",
-                "IonInspector.reports.context_processors.active_nav"
+                "IonInspector.reports.context_processors.active_nav",
             ],
         },
     },
 ]
 
-MEDIA_ROOT = '/var/lib/inspector/media/'
-MEDIA_URL = '/media/'
+MEDIA_ROOT = "/var/lib/inspector/media/"
+MEDIA_URL = "/media/"
 
 CELERY_IGNORE_RESULT = False
-CELERY_TASK_SERIALIZER = 'pickle'
-CELERY_ACCEPT_CONTENT = ['pickle', 'json']
-CELERY_RESULT_BACKEND = 'rpc'
-CELERY_RESULT_SERIALIZER = 'pickle'
+CELERY_TASK_SERIALIZER = "pickle"
+CELERY_ACCEPT_CONTENT = ["pickle", "json"]
+CELERY_RESULT_BACKEND = "rpc"
+CELERY_RESULT_SERIALIZER = "pickle"
 SITE_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 
-VERSION = '1.8.0-rc.2'
+VERSION = "1.8.0-rc.3"
 
 RAVEN_CONFIG = {
-    'dsn': 'http://d8a6a72730684575afc834c95ebbdc60:1e5b396140654efd9b3361401f530204@sentry.itw//11',
-    'release': VERSION
+    "dsn": "http://d8a6a72730684575afc834c95ebbdc60:1e5b396140654efd9b3361401f530204@sentry.itw//11",
+    "release": VERSION,
 }
 
 try:
