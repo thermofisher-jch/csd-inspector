7a6a995bd19b715bcd0273ce69f4e29683922298
John Heinnickel <john.heinnickel@thermofisher.com>
2022-04-02 03:22:32 -0700

    Version bump

--- IonInspector/settings.py
+++ IonInspector/settings.py
@@ -170,7 +170,7 @@ CELERY_RESULT_SERIALIZER = "pickle"
 SITE_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 
-VERSION = "1.9.0-rc.1"
+VERSION = "1.9.0-rc.2"
 try:
     # Allows contextual override of displayed version tag
     with open("/var/lib/inspector/version", "r") as fd:
